2c472b0d73429b86354b9d5d1ebafe60140c4275
jun-16 <sumitomo.firebase@gmail.com>
2022-09-23 18:27:28 +0900

    追加したメモをリアルタイムで取得可能に

--- lib/pages/top_page.dart
+++ lib/pages/top_page.dart
@@ -15,58 +15,49 @@ class TopPage extends StatefulWidget {
 }
 
 class _TopPageState extends State<TopPage> {
-  List<Memo> memoList = [];
-
-  Future<void> fetchMemo() async{
-    final memoCollection = await FirebaseFirestore.instance.collection('memo').get();
-    final docs = memoCollection.docs;
-    print('データ');
-    print(memoCollection);
-    print(docs);
-    for(var doc in docs) {
-      Memo fetchMemo = Memo(
-        title: doc.data()['title'],
-        detail: doc.data()['detail'],
-        createdDate: doc.data()['createdDate']
-        // title: 'title',
-        // detail: 'detail',
-        // createdDate: Timestamp.now()
-      );
-      memoList.add(fetchMemo);
-    }
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    fetchMemo();
-  }
+  final memoCollection = FirebaseFirestore.instance.collection('memo');
 
   @override
   Widget build(BuildContext context) {
-    print('memoList：');
-    print(memoList);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter × Firebase'),
       ),
-      body: ListView.builder(
-        itemCount: memoList.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(memoList[index].title),
-            onTap: () {
-              //確認画面に遷移する記述を書く
-              Navigator.push(context, MaterialPageRoute(
-                builder: (context) => MemoDetailPage(memoList[index])));
-            },
+      body: StreamBuilder<QuerySnapshot>(
+        stream: memoCollection.snapshots(),
+        builder: (context, snapshot) {
+          if(snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          }
+          if(!snapshot.hasData) {
+            return const Center(child: Text('データがありません'));
+          }
+          final docs = snapshot.data!.docs;
+          return ListView.builder(
+            itemCount: docs.length,
+            itemBuilder: (context, index) {
+              Map<String, dynamic> data = docs[index].data() as Map<String, dynamic>;
+              final Memo fetchMemo = Memo(
+                title: data['title'],
+                detail: data['detail'],
+                createdDate: data['createdDate'],
+                updatedDate: data['updatedDate'],
+              );
+              return ListTile(
+                title: Text(fetchMemo.title),
+                onTap: () {
+                  //確認画面に遷移する記述を書く
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) => MemoDetailPage(fetchMemo)));
+                },
+              );
+            }
           );
         }
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) => AddMemoPage()));
+          Navigator.push(context, MaterialPageRoute(builder: (context) => const AddMemoPage()));
         },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
